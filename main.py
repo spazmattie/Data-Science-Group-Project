b4738c831c6e87723ff70810d3c9822aac7a53e0
spazmattie <spazmattie@gmail.com>
2020-07-22 16:54:02 -0400

    Added a print statement

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__, static_url_path='', static_folder='')
 def root():
     return app.send_static_file('event.html')
 
+print ("My name is Matthew Smith")
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
     
