66ce761a8cfb3cd9cdabcd6a419921ea279acc81
spazmattie <spazmattie@gmail.com>
2020-07-22 16:40:03 -0400

    First Github Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("----------------------------------")
+  let colors = ["red", "green", "blue"];
+  console.log(colors);
+
+console.log("This is some practice text")
+  let names = ["Matthew", "Samantha", "Steven"];
+  console.log(names)
---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_url_path='', static_folder='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('event.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    
