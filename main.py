c6891592d167bd370c9410d1d92cd476e0d670c4
spazmattie <spazmattie@gmail.com>
2020-07-22 20:47:15 -0400

    penny.html

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='', static_folder='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return app.send_static_file("penny.html")
 
 print ("My name is Matthew Smith")
 
