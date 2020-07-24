3f955d0d7a01604ba7044bb1c52f98bafee8a336
spazmattie <spazmattie@gmail.com>
2020-07-24 09:25:48 -0400

    I tried LMAOOO

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__, static_url_path='', static_folder='')
 def root():
     return app.send_static_file("index.html")
 
+"""
 @app.route('/penny')
 def penny():
     return app.send_static_file("penny.html")
@@ -23,3 +24,4 @@ print ("My name is Matthew Smith")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+    """
