f1fa426b21ed1db79b0a8f90fe7d8c9097f91bda
spazmattie <spazmattie@gmail.com>
2020-07-24 11:41:03 -0400

    Class from yesterday

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 
-from flask import Flask, request
+from flask import Flask, request, render_template, make_response
 
-app = Flask(__name__, static_url_path='', static_folder='')
+app = Flask(__name__, static_url_path='/index', static_folder='')
 
 @app.route('/')
-def root():
-    return app.send_static_file("index.html")
-
-"""
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    
 @app.route('/penny')
 def penny():
     return app.send_static_file("penny.html")
@@ -20,8 +31,15 @@ def name():
 def event():
     return app.send_static_file("event.html")
 
+@app.route('/hello')
+def hello():
+    name = "Matthew"
+    html = "<html><title>Welcome Page</title><body>Hello "
+    html += name
+    html += "</body></html>"
+    return make_response(html)
+
 print ("My name is Matthew Smith")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
-    """
